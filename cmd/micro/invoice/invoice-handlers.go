4c06d8b555756a7cab83f1cab35283e9c52078f6
Natsumag <ntmg0906@gmail.com>
2023-11-10 07:18:31 +0900

    add to create pdf func

--- cmd/micro/invoice/invoice-handlers.go
+++ cmd/micro/invoice/invoice-handlers.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"github.com/phpdave11/gofpdf"
+	"github.com/phpdave11/gofpdf/contrib/gofpdi"
 	"net/http"
 	"time"
 )
@@ -26,6 +28,12 @@ func (app *application) CreateAndSendInvoice(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
+	err = app.createInvoicePDF(order)
+	if err != nil {
+		app.badRequest(w, r, err)
+		return
+	}
+
 	var resp struct {
 		Error   bool   `json:"error"`
 		Message string `json:"message"`
@@ -34,4 +42,37 @@ func (app *application) CreateAndSendInvoice(w http.ResponseWriter, r *http.Requ
 	resp.Message = fmt.Sprintf("Invoice %d.pdf created and sent to %s", order.ID, order.Email)
 
 	app.writeJSON(w, http.StatusCreated, resp)
+}
+
+func (app *application) createInvoicePDF(order Order) error {
+	pdf := gofpdf.New("P", "mm", "A4", "")
+	pdf.SetMargins(10, 13, 10)
+	pdf.SetAutoPageBreak(true, 0)
+
+	importer := gofpdi.NewImporter()
+	t := importer.ImportPage(pdf, "./pdf-templates/invoice.pdf", 1, "/MediaBox")
+
+	pdf.AddPage()
+	importer.UseImportedTemplate(pdf, t, 0, 0, 215, 0)
+	pdf.SetXY(10, 50)
+	pdf.SetFont("Times", "", 11)
+	pdf.CellFormat(100, 10, fmt.Sprintf("%s %s", order.FirstName, order.LastName), "", 0, "L", false, 0, "")
+	pdf.Ln(5)
+	pdf.CellFormat(100, 10, order.Email, "", 0, "L", false, 0, "")
+	pdf.Ln(5)
+	pdf.CellFormat(100, 10, order.CreatedAt.Format("2006-01-02"), "", 0, "L", false, 0, "")
+	pdf.SetXY(60, 100)
+	pdf.CellFormat(150, 10, order.Product, "", 0, "L", false, 0, "")
+	pdf.SetX(170)
+	pdf.CellFormat(20, 10, fmt.Sprintf("%d", order.Quantity), "", 0, "C", false, 0, "")
+	pdf.SetX(180)
+	pdf.CellFormat(20, 10, fmt.Sprintf("￥%d", order.Amount), "", 0, "R", false, 0, "")
+
+	invoicePath := fmt.Sprintf("./invoices/%d.pdf", order.ID)
+	err := pdf.OutputFileAndClose(invoicePath)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
