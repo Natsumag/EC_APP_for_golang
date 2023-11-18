c0cd29a0c9ba2b39563b7810668e48abc7af4e7b
Natsumag <ntmg0906@gmail.com>
2023-11-19 00:06:42 +0900

    add to move all config info in config.go from api.go

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,12 +1,50 @@
 package config
 
+import (
+	"os"
+	"strconv"
+)
+
 type Config struct {
+	Port   int
+	Env    string
+	DB     struct{ DSN string }
+	Stripe struct{ Secret, Key string }
+	SMTP   struct {
+		Host, Username, Password, FromMail string
+		Port                               int
+	}
+	SecretKey   string
+	WebURL      string
+	MicroURL    string
 	Status      map[string]int
 	IsRecurring map[string]int
 }
 
 func LoadConfig() Config {
+	port, _ := strconv.Atoi(os.Getenv("API_PORT"))
+	smtpport, _ := strconv.Atoi(os.Getenv("SMTP_PORT"))
+	dsn := os.Getenv("DB_USER") + ":" + os.Getenv("DB_PASSWORD") + "@tcp(" + os.Getenv("DB_HOST") + ":" + os.Getenv("DB_PORT") + ")/" + os.Getenv("DB_NAME") + "?parseTime=true&tls=false"
+
 	return Config{
+		Port: port,
+		Env:  "development", // Default value if not provided
+		DB: struct{ DSN string }{
+			DSN: dsn,
+		},
+		SMTP: struct {
+			Host, Username, Password, FromMail string
+			Port                               int
+		}{
+			Host:     os.Getenv("SMTP_HOST"),
+			Username: os.Getenv("SMTP_USER"),
+			Password: os.Getenv("SMTP_PASSWORD"),
+			FromMail: os.Getenv("SMTP_FROM_MAIL"),
+			Port:     smtpport,
+		},
+		SecretKey: os.Getenv("SECRETKEY"),
+		WebURL:    os.Getenv("WEB_URL"),
+		MicroURL:  os.Getenv("MICRO_URL"),
 		Status: map[string]int{
 			"Cleared":   1,
 			"Refunded":  2,
